0b4ed3b843b55cae22f2fde1b5550bb6ef9dc383
enaut <enaut.irakasle@gmail.com>
2018-01-26 13:06:45 +0100

    lehen commita

---
+++ BBDD/biblioteca.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 26-01-2018 a las 12:36:27
+-- Versión del servidor: 10.1.30-MariaDB
+-- Versión de PHP: 7.2.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `biblioteca`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `apellido` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `edad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `edad`) VALUES
+(1, 'jon', 'jauregi', 21),
+(2, 'jon imanol', 'pinto', 22),
+(3, 'mauricio', 'diaz', 19);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ src/Main.java
@@ -0,0 +1,55 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		try {
+			//se carga el diver
+			Class.forName("com.mysql.jdbc.Driver");
+			//crear la connexion con la BBDD biblioteca
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost/biblioteca", "root", "");
+			
+			//statement para ejecutar querys
+			Statement st = con.createStatement();
+			
+			//ejecutar query
+			ResultSet rs = st.executeQuery("select * from usuarios");
+			
+			//imprimir en pantalla el resultado de la consulta
+			while (rs.next()) {
+				System.out.println(rs.getInt("id") + " "
+						+ rs.getString("nombre")
+						+ " " + rs.getString("apellido") + " "
+						+ rs.getInt("edad"));
+			}
+
+			//ejecutar un insert
+			//st.execute("INSERT INTO usuarios(nombre, apellido, edad) VALUES ('mauricio', 'diaz', 19)");
+
+			//pedir datos por pantalla
+			Scanner scan = new Scanner(System.in);
+			System.out.println("Introduce un nombre");
+			String nombre = scan.nextLine();
+			System.out.println("Introduce un apellido");
+			String apellido = scan.nextLine();
+			System.out.println("Introduce una edad");
+			int edad = Integer.parseInt(scan.nextLine());
+			
+	
+
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
