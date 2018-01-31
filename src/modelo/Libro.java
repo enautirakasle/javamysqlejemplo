0547f04ec40642c3bf555b9976e3926f071e9e34
enaut <enaut.irakasle@gmail.com>
2018-01-31 10:47:39 +0100

    anadir metodos a los modelos

---
+++ src/modelo/Libro.java
@@ -0,0 +1,32 @@
+package modelo;
+
+public class Libro {
+	private int id;
+	private String titulo;
+	private String autor;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+}
