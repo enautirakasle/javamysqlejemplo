1034c8174c859e9b52dc639d812dea66ba109dcb
enaut <enaut.irakasle@gmail.com>
2018-01-31 10:46:55 +0100

    quitar comentarios TODO

--- src/modelo/Conector.java
+++ src/modelo/Conector.java
@@ -10,15 +10,12 @@ public class Conector {
 	/**
 	 */
 	Conector() {
-		
 			try {
 				Class.forName("com.mysql.jdbc.Driver");
 				this.conexion = DriverManager.getConnection("jdbc:mysql://" + Config.HOST + "/" + Config.BBDD, Config.USERNAME, Config.PASSWORD);
 			} catch (ClassNotFoundException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			} catch (SQLException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			
