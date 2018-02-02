811800f832fab4e8d45b30ee31b29758e1f8548c
enaut <enaut.irakasle@gmail.com>
2018-02-02 12:47:13 +0100

    usuario menuaren proba

---
+++ src/MainUsuario.java
@@ -0,0 +1,10 @@
+import vista.UsuarioVista;
+
+public class MainUsuario {
+
+	public static void main(String[] args) {
+		UsuarioVista usuarioVista = new UsuarioVista();
+		usuarioVista.menuDeUsuario();
+	}
+
+}
