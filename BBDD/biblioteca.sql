6ea65213279b266a829d77fe56ab27842adcb068
eñaut <enaut.irakasle@gmail.com>
2018-03-13 08:47:00 +0100

    nuevos datos y tablas

--- BBDD/biblioteca.sql
+++ BBDD/biblioteca.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 26-01-2018 a las 12:36:27
+-- Tiempo de generación: 13-03-2018 a las 08:45:51
 -- Versión del servidor: 10.1.30-MariaDB
 -- Versión de PHP: 7.2.1
 
@@ -25,6 +25,52 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `libros`
+--
+
+CREATE TABLE `libros` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `autor` varchar(32) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `libros`
+--
+
+INSERT INTO `libros` (`id`, `titulo`, `autor`) VALUES
+(1, 'Mushé', 'kirmen'),
+(2, 'libro1', 'autor1'),
+(3, 'libro2', 'autor1');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `prestamos`
+--
+
+CREATE TABLE `prestamos` (
+  `id` int(11) NOT NULL,
+  `id_libro` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `fecha_prestamo` date NOT NULL,
+  `fecha_limite` date NOT NULL,
+  `entregado` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `prestamos`
+--
+
+INSERT INTO `prestamos` (`id`, `id_libro`, `id_usuario`, `fecha_prestamo`, `fecha_limite`, `entregado`) VALUES
+(1, 2, 15, '2018-02-14', '2018-03-07', 1),
+(2, 2, 1, '2018-02-21', '2018-03-14', 0),
+(5, 1, 1, '2018-02-20', '2018-03-13', 0),
+(6, 3, 9, '2018-02-13', '2018-03-06', 0);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuarios`
 --
 
@@ -32,21 +78,49 @@ CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
   `nombre` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
   `apellido` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
-  `edad` int(11) NOT NULL
+  `edad` int(11) NOT NULL,
+  `dni` varchar(9) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `fecha_nacimiento` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `edad`) VALUES
-(1, 'jon', 'jauregi', 21),
-(2, 'jon imanol', 'pinto', 22),
-(3, 'mauricio', 'diaz', 19);
+INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `edad`, `dni`, `fecha_nacimiento`) VALUES
+(1, 'jon', 'jauregi', 21, '', '0000-00-00'),
+(2, 'jon imanol', 'pinto', 22, '', '0000-00-00'),
+(3, 'mauricio', 'diaz', 19, '', '0000-00-00'),
+(4, 'aaaa', 'ppppp', 23, '', '0000-00-00'),
+(5, 'Mikel', 'Badiola', 33, '', '0000-00-00'),
+(6, 'Mikel', 'Badiola', 33, '', '0000-00-00'),
+(7, 'joxe', 'zabala', 23, '', '0000-00-00'),
+(8, 'mikel', 'arrizabalaga', 0, '', '0000-00-00'),
+(9, 'joxe', 'arrizabalaga', 23, '', '0000-00-00'),
+(10, 'mikel', 'zabaleta', 35, '', '0000-00-00'),
+(11, 'xabier', 'arana', 30, NULL, NULL),
+(12, 'enaut', 'agirre', 34, NULL, '1983-06-25'),
+(13, 'qqq', 'www', 23, NULL, '0191-01-12'),
+(14, 'qqqq', 'wwww', 23, NULL, '0012-03-01'),
+(15, 'we', 'we', 23, '11111111a', '2012-12-12');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `libros`
+--
+ALTER TABLE `libros`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `prestamos`
+--
+ALTER TABLE `prestamos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_libro` (`id_libro`),
+  ADD KEY `id_usuairo` (`id_usuario`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -59,10 +133,33 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `libros`
+--
+ALTER TABLE `libros`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `prestamos`
+--
+ALTER TABLE `prestamos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `prestamos`
+--
+ALTER TABLE `prestamos`
+  ADD CONSTRAINT `prestamos_ibfk_1` FOREIGN KEY (`id_libro`) REFERENCES `libros` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `prestamos_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
